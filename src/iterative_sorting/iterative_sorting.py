1964e0da9414adee733401fe6a73f6d1e6d1eaef
spenclark <spenclark@hotmail.com>
2020-03-17 12:43:20 -0700

    fixed inner loop error

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,34 +1,35 @@
 arr = [6, 3, 9, 1, 3, 7, 2]
-print(f'before iterative sort', arr)
+print('before', arr)
 
 
 def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        for j in range(smallest_index + 1, len(arr)):
-            if arr[j] < arr[cur_index]:
-                cur_index = j
-        if cur_index != smallest_index:
-            arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+    ranger = len(arr) - 1
+    for i in range(0, ranger):
+        discovered_floor = i
+        for j in range(i, ranger + 1):
+            if arr[j] < arr[discovered_floor]:
+                discovered_floor = j
 
-        # TO-DO: swap
+        temp_index = arr[i]
+        arr[i] = arr[discovered_floor]
+        arr[discovered_floor] = temp_index
+
     return arr
 
 
-print(f'after iterative sort', selection_sort(arr))
+print('after', selection_sort(arr))
 
 # TO-DO:  implement the Bubble Sort function below
 
 
 def bubble_sort(arr):
-    for i in range(0, len(arr) - 1):
-        for j in range(0, len(arr) - 1 - i):
+    ranger = len(arr) - 1
+    for i in range(0, ranger):
+        for j in range(0, ranger - i):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                temp_index = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp_index
     return arr
 
 
@@ -37,5 +38,7 @@ print("BUBBLE SORT", bubble_sort(arr))
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
-
     return arr
+
+
+print("COUNT SORT", count_sort(arr))
