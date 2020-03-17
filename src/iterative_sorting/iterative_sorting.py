2803c7d908c8d647deac38b9c3486c972be5c09b
spenclark <spenclark@hotmail.com>
2020-03-17 08:22:17 -0700

    selecton sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,7 @@
-# TO-DO: Complete the selection_sort() function below
+arr = [6, 3, 9, 1, 3, 7, 2]
+print(f'before iterative sort', arr)
+
+
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -6,25 +9,30 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
+        for j in range(smallest_index + 1, len(arr)):
+            if arr[j] < arr[cur_index]:
+                cur_index = j
+        if cur_index != smallest_index:
+            arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
         # TO-DO: swap
-
     return arr
 
+
+print(f'after iterative sort', selection_sort(arr))
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort(arr):
-    swap_occurred = True
-    while swap_occurred:
-        swap_occurred = False
-        for i in range(0, len(arr) - 1):
-            if arr[i] > arr[i + 1]:
-                temp_number = arr[i]
-                arr[i] = arr[i + 1]
-                arr[i + 1] = temp_number
-                swap_occurred = True
 
+
+def bubble_sort(arr):
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
+
+print("BUBBLE SORT", bubble_sort(arr))
 
 
 # STRETCH: implement the Count Sort function below
